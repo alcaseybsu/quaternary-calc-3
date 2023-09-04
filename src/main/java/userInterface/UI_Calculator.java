f3a894ad85ff429b9f815176e304f2904f47ccec
hannahj001 <hannahjohnstongk@gmail.com>
2023-09-04 15:45:16 -0400

    UI changes and toggle button

--- src/main/java/userInterface/UI_Calculator.java
+++ src/main/java/userInterface/UI_Calculator.java
@@ -3,7 +3,7 @@ package userInterface;
 import javafx.scene.control.Label;
 
 public class UI_Calculator {
-    public Label decimal_result;
-    public Label quaternary_result;
-    public Label equation_window;
+
+
 }
+
