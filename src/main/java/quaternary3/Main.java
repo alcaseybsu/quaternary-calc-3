8690a72eb5d411a14117eab9728f98e19ac6af15
hannahj001 <hannahjohnstongk@gmail.com>
2023-09-04 14:55:44 -0400

    basic layout for UI

--- src/main/java/quaternary3/Main.java
+++ src/main/java/quaternary3/Main.java
@@ -1,18 +1,24 @@
 package quaternary3;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
 public class Main extends Application {
-    public void start(Stage stage) {
-        Group root = new Group();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Calculator.fxml")));
         Scene scene = new Scene(root);
         stage.setTitle("QuanCalc");
         stage.setScene(scene);
         stage.show();
-        System.out.println("Hello world!");
+
+
+
     }
 
     public static void main(String[] args) { launch(args); }
