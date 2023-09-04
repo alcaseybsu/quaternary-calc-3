7b1a02d54c589d41d187c618ec6463387a429d1f
hannahj001 <hannahjohnstongk@gmail.com>
2023-09-04 14:45:08 -0400

    basic layout for UI

--- src/main/java/userInterface/UI_Calculator.java
+++ src/main/java/userInterface/UI_Calculator.java
@@ -1,4 +1,9 @@
 package userInterface;
 
+import javafx.scene.control.Label;
+
 public class UI_Calculator {
+    public Label decimal_result;
+    public Label quaternary_result;
+    public Label equation_window;
 }
