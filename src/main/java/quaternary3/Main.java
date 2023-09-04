6cab478bc2cfb8422793723d9c6ac9035c69aedd
caige <caigerogers12@gmail.com>
2023-09-04 14:10:47 -0400

    SceneStart, Added the basic Scene

--- src/main/java/quaternary3/Main.java
+++ src/main/java/quaternary3/Main.java
@@ -1,8 +1,19 @@
 package quaternary3;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        stage.setTitle("QuanCalc");
+        stage.setScene(scene);
+        stage.show();
         System.out.println("Hello world!");
     }
+
+    public static void main(String[] args) { launch(args); }
 }
