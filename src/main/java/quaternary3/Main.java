9a4891154a3a492ca10ef969b8efe50e00ae77cf
caige <caigerogers12@gmail.com>
2023-09-04 14:59:00 -0400

    Added Buttons to GUI Calculator

--- src/main/java/quaternary3/Main.java
+++ src/main/java/quaternary3/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/Calculator.fxml")));
         Scene scene = new Scene(root);
-        stage.setTitle("QuanCalc");
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
     }
