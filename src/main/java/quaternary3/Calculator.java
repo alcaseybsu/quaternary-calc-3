2461eb00da2d4e1b387597029d54c0f7ef4ee530
Samuel Gross <52175733+Haitifan03@users.noreply.github.com>
2023-09-05 22:31:56 -0400

    I added in the structure for all of the methods we will need to test.

--- src/main/java/quaternary3/Calculator.java
+++ src/main/java/quaternary3/Calculator.java
@@ -1,17 +1,57 @@
 package quaternary3;
 
+import java.util.Objects;
+
 public class Calculator {
     public Calculator(){}
 
     public int calculate(int firstNum, String Operator, int secondNum){
 
+        if (Objects.equals(Operator, "*")){
+            return multiply(firstNum, secondNum);
+        }
+        else if (Objects.equals(Operator, "/")){
+            return divide(firstNum, secondNum);
+        }
+        else if (Objects.equals(Operator, "-")){
+            return subtract(firstNum, secondNum);
+        }
+        else{
+            return add(firstNum, secondNum);
+        }
+    }
 
+
+    public int calculate(int firstNum, String Operator){
+        if (Objects.equals(Operator, "root")){
+            return root(firstNum);
+        }
+        else {
+            return squareroot(firstNum);
+        }
+    }
+
+    private int squareroot(int firstNum) {
         return 0;
     }
 
-    public int calculate(int firstNum, String Operator){
+    private int root(int firstNum) {
+        return 0;
+    }
 
+    private int subtract(int firstNum, int secondNum) {
+        return 0;
+    }
 
+    private int add(int firstNum, int secondNum) {
+        return 0;
+    }
+
+    private int divide(int firstNum, int secondNum) {
+        return 0;
+    }
+
+    private int multiply(int firstNum, int secondNum) {
         return 0;
     }
 
