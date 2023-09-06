52685820e7917c10528ee387fc17234b36192e6d
hannahj001 <hannahjohnstongk@gmail.com>
2023-09-06 14:39:47 -0400

    Simple Button Additions

--- src/main/java/userInterface/UI_Calculator.java
+++ src/main/java/userInterface/UI_Calculator.java
@@ -1,72 +1,74 @@
 package userInterface;
 
-import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
 public class UI_Calculator {
     public TextField equation_window;
     public TextField result_window;
-
     public String equation ="";
 
-    public void buttonClearClicked(ActionEvent actionEvent) {
+    public void buttonClearClicked() {
         equation = "";
         equation_window.setText(equation);
     }
 
-    public void buttonSquareClicked(ActionEvent actionEvent) {
-
+    public void buttonSquareClicked() {
+        equation += "²";
+        equation_window.setText(equation);
     }
 
-    public void buttonSquareRootClicked(ActionEvent actionEvent) {
-        equation += 0 ;
+    public void buttonSquareRootClicked() {
+        equation += "√" ;
         equation_window.setText(equation);
 
     }
-    public void buttonEnterClicked(ActionEvent actionEvent) {
+    public void buttonEnterClicked() {
 
     }
 
-    public void buttonPlusClicked(ActionEvent actionEvent) {
+    public void buttonPlusClicked() {
         equation += "+";
         equation_window.setText(equation);
 
     }
 
-    public void buttonMinusClicked(ActionEvent actionEvent) {
+    public void buttonMinusClicked() {
         equation += "-";
         equation_window.setText(equation);
     }
 
-    public void buttonMultiplyClicked(ActionEvent actionEvent) {
+    public void buttonMultiplyClicked() {
         equation += "*";
         equation_window.setText(equation);
     }
 
-    public void buttonDivideClicked(ActionEvent actionEvent) {
+    public void buttonDivideClicked() {
         equation += "/";
         equation_window.setText(equation);
     }
 
-    public void button0Clicked(ActionEvent actionEvent) {
+    public void button0Clicked() {
         equation += 0;
         equation_window.setText(equation);
 
     }
-    public void button1Clicked(ActionEvent actionEvent) {
+    public void button1Clicked() {
         equation += 1;
         equation_window.setText(equation);
     }
 
-    public void button2Clicked(ActionEvent actionEvent) {
+    public void button2Clicked() {
         equation += 2;
         equation_window.setText(equation);
     }
 
-    public void button3Clicked(ActionEvent actionEvent) {
+    public void button3Clicked() {
         equation += 3;
         equation_window.setText(equation);
     }
 
+    public void toggleButtonPressed() {
+
+    }
 }
 
