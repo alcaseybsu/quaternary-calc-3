1a14a539aa01979ae833a93b211f26656d92d61e
hannahj001 <hannahjohnstongk@gmail.com>
2023-09-06 14:11:54 -0400

    Creation of Button actions

--- src/main/java/userInterface/UI_Calculator.java
+++ src/main/java/userInterface/UI_Calculator.java
@@ -1,10 +1,46 @@
 package userInterface;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
 public class UI_Calculator {
     public TextField equation_window;
     public TextField result_window;
 
+    public void buttonClearClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonSquareClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonSquareRootClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonEnterClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonPlusClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMinusClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonMultiplyClicked(ActionEvent actionEvent) {
+    }
+
+    public void buttonDivideClicked(ActionEvent actionEvent) {
+    }
+
+    public void button0Clicked(ActionEvent actionEvent) {
+    }
+
+    public void button1Clicked(ActionEvent actionEvent) {
+    }
+
+    public void button2Clicked(ActionEvent actionEvent) {
+    }
+
+    public void button3Clicked(ActionEvent actionEvent) {
+    }
 }
 
