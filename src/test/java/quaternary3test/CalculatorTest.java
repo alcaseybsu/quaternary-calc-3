10ec5c6e8dcef1a46618aad472f63449fad4554e
Leah <alcasey@bsu.edu>
2023-09-08 00:25:23 -0400

    Update CalculatorTest.java

--- src/test/java/quaternary3test/CalculatorTest.java
+++ src/test/java/quaternary3test/CalculatorTest.java
@@ -1,7 +1,5 @@
 package quaternary3test;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -13,4 +11,39 @@ class CalculatorTest {
     @Test
     void testCalculate() {
     }
-}
+    
+    @Test
+    public void testAddition() {
+        String result = Calculator.calculate(10, "+", 5);
+        assertEquals("23", result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        String result = Calculator.calculate(10, "-", 5);
+        assertEquals("5", result);
+    }
+
+    @Test
+    public void testMultiplication() {
+        String result = Calculator.calculate(10, "x", 5);
+        assertEquals("32", result);
+    }
+
+    @Test
+    public void testDivision() {
+        String result = Calculator.calculate(10, "/", 5);
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void testSquare() {
+        String result = Calculator.calculate(4, "square");
+        assertEquals("20", result);
+    }
+
+    @Test
+    public void testSquareRoot() {
+        String result = Calculator.calculate(16, "root");
+        assertEquals("2", result);
+    }
